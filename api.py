b765632cb62cb732f05e500fbfb00379a033c15b
Jenn <um.jennifer@gmail.com>
2022-10-25 21:45:32 -0700

    working GET

---
+++ api.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import random
+import json
+
+with open('exercises.json') as file:
+   exercises_data = json.load(file)
+
+print("exercises_data=",len(exercises_data))
+
+app = Flask(__name__)
+api = Api(app)
+
+def generate_prn():
+    random.seed()
+    len_exercises_data= len(exercises_data)
+    index = random.randrange(1, 100)
+    if index >= len_exercises_data:
+            index = int(index) % len_exercises_data
+    return str(index)
+
+
+class Exercises(Resource):
+    def get(self):
+        # Default to 200 OK
+        return exercises_data[str(generate_prn())]
+
+api.add_resource(Exercises, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
